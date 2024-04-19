d2b9371141dd3874c4ee9f14f3403b23f50944e6
Enakarhire Ovie Louis <enakarhireovie7@example.com>
2024-04-19 14:29:36 -0700

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-file { "/tmp/school":
-  ensure  => "present",
-  mode    => "0744",
-  owner   => "www-data",
-  group   => "www-data",
-  content => "I love Puppet",
+#The code will create a file name school inside the /temp directory
+file { '/tmp/school':
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
