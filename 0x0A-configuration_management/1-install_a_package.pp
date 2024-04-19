69c7a84f264fa6dcf963da1dea9baa62417da0a5
Enakarhire Ovie Louis <enakarhireovie7@example.com>
2024-04-19 14:38:43 -0700

    task1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,10 @@
-# puppet_flask_install.pp
+#install flask package from pip3
 
-package { 'python3-pip':
-  ensure => installed,
+package {'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
+package {'werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3',
 }
