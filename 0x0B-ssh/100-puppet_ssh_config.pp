a90debbe8d285784bbf6d8af68459a4b316dee59
Enakarhire Ovie Louis <enakarhireovie7@example.com>
2024-04-21 21:31:01 -0700

    task

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# using pippet to make changes to our configuration file
+
+file { 'ect/ssh/ssh_config':
+        ensure => present,
+content =>"
+        #SSH client configuration
+        host*
+        IdentityFile ~/.ssh/school
+        PasswordAuthentication no
+}
